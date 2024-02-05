86900a24baf7e5fa36964cae29a67513c44debf2
Maheswaranx15 <maheshwaran66648@gmail.com>
2024-02-05 13:40:17 +0530

    testcases added

--- test/test.js
+++ test/test.js
@@ -1,49 +1,54 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
-const { ethers } = require("hardhat");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const {
+  BN,           // Big Number support
+  constants,    // Common constants, like the zero address and largest integers
+  expectEvent,  // Assertions for emitted events
+  expectRevert, // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
+require("@nomiclabs/hardhat-truffle5");
+
 
 describe("EscrowContract", function () {
-  let EscrowContract;
-  let escrowContract;
-  let owner;
-  let buyer;
-  let seller;
-  let arbitrator;
-  let token;
+  let tokenInstance;
+  let escrowContractinstance;
 
-  it("Deployment",async ()=>{
+  it("Contract Deployment",async ()=>{
+
     [owner, buyer, seller, arbitrator] = await ethers.getSigners();
     const Token = await ethers.getContractFactory("Token20"); //ERC20 contract
-    let tokenInstance = await Token.deploy(owner.address);
+    tokenInstance = await Token.deploy(owner.address);
     await tokenInstance.deployed()
-    EscrowContract = await ethers.getContractFactory("SaleP2P");
-    let escrowContractinstance = await EscrowContract.deploy();
-    await escrowContractinstance.deployed()
+
+    console.log(tokenInstance.address)
+
+    const EscrowContract = await ethers.getContractFactory("SaleP2P");
+    escrowContractinstance = await EscrowContract.deploy();
+    await escrowContractinstance.deployed();
+
+    console.log(escrowContractinstance.address)
   })
 
-  // it("should create an agreement", async function () {
-  //   await token
-  //     .connect(owner)
-  //     .transfer(buyer.address, ethers.utils.parseEther("1"));
-  //   var tx = await escrowContract
-  //     .connect(buyer)
-  //     .createAgreement(
-  //       seller.address,
-  //       arbitrator.address,
-  //       ethers.utils.parseEther("1"),
-  //       token.address
-  //     );
-  //   var txn = await tx.wait();
-  //   const agreementId = txn.events[0].args["agreementId"];
-  //   const agreement = await escrowContract.agreements(agreementId);
+  it("should create an agreement", async function () {
+    await token.connect(owner).transfer(buyer.address, ethers.utils.parseEther("1"));
+    var tx = await escrowContract.connect(buyer)
+      .createAgreement(
+        seller.address,
+        arbitrator.address,
+        ethers.utils.parseEther("1"),
+        token.address
+    );
+    var txn = await tx.wait();
+    const agreementId = txn.events[0].args["agreementId"];
+    const agreement = await escrowContract.agreements(agreementId);
 
-  //   expect(agreement.buyer).to.equal(buyer.address);
-  //   expect(agreement.seller).to.equal(seller.address);
-  //   expect(agreement.arbitrator).to.equal(arbitrator.address);
-  //   expect(agreement.amount).to.equal(ethers.utils.parseEther("1"));
-  //   expect(agreement.state).to.equal(0); 
-  //   expect(agreement.token).to.equal(token.address);
-  // });
+    expect(agreement.buyer).to.equal(buyer.address);
+    expect(agreement.seller).to.equal(seller.address);
+    expect(agreement.arbitrator).to.equal(arbitrator.address);
+    expect(agreement.amount).to.equal(ethers.utils.parseEther("1"));
+    expect(agreement.state).to.equal(0); 
+    expect(agreement.token).to.equal(token.address);
+  });
 
   // it("should pay for an agreement", async function () {
   //   await token
